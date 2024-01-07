239f02cda9bd9d35a7bb741941e071231b3d102f
Anlor <chenxiex@outlook.com>
2024-01-07 15:50:03 +0800

    调整了模型结构，增加了一些层。

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ class CNNClassifier(nn.Module):
         super(CNNClassifier, self).__init__()
         self.conv1=nn.Conv2d(3,32,3,1)
         self.conv2=nn.Conv2d(32,64,3,1)
+        self.conv3=nn.Conv2d(64,128,3,1)
         self.dropout1=nn.Dropout(0.25)
         self.dropout2=nn.Dropout(0.5)
-        self.fc1=nn.Linear(14*14*64,128)
-        self.fc2=nn.Linear(128,10)
+        self.fc1=nn.Linear(6*6*128,1024)
+        self.fc2=nn.Linear(1024,128)
+        self.fc3=nn.Linear(128,10)
     
     def forward(self,x):
         x=self.conv1(x)
@@ -24,12 +26,16 @@ class CNNClassifier(nn.Module):
         x=self.conv2(x)
         x=F.relu(x)
         x=F.max_pool2d(x,2)
-        x=self.dropout1(x)
+        x=self.conv3(x)
+        x=F.relu(x)
+        x=F.max_pool2d(x,2)
         x=torch.flatten(x,1)
         x=self.fc1(x)
         x=F.relu(x)
         x=self.dropout2(x)
         x=self.fc2(x)
+        x=F.relu(x)
+        x=self.fc3(x)
         output=F.log_softmax(x,dim=1)
         return output
 
